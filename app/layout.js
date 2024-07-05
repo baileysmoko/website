feffacbd19ac2800e7b100b6e28d1262f968bfb4
Bailey Smoko <bailey_smoko@brown.edu>
2024-07-05 15:53:49 -0400

    favicon

--- app/layout.js
+++ app/layout.js
@@ -7,17 +7,25 @@ import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
 import ScrollToTop from "./components/helper/scroll-to-top";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Bailey Smoko",
-  description:
-    "Hi! I'm Bailey. I'm a rising junior at Brown University, studying Statistics and CS. I am passionate about mathematics, problem-solving, programming, and promoting sustainability. I love tackling complex challenges and leveraging technology to create innovative solutions.",
+  // ... other metadata
+  icons: {
+    icon: [
+      { url: '/favicon.ico', type: 'image/x-icon' },
+      { url: '/favicon.png', type: 'image/png' },
+    ],
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+<head>
+  <link rel="icon" type="image/x-icon" href="/favicon.ico?v=3" />
+  <link rel="icon" type="image/png" href="/favicon.png?v=3" />
+</head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
